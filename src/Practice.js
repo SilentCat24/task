cc7a1a758a846eba7065549e3ac33746025e6f7c
Arun R Kumar <arunmahi921@gmail.com>
2022-08-12 14:15:46 +0530

    meeting

--- src/Practice.js
+++ src/Practice.js
@@ -13,10 +13,17 @@ const App = () => {
   const [displayData, setDisplayData] = useState({});
   const auditNames = ["Audit1", "Audit2", "Audit3"];
   const documentName = ["Doc1", "Doc2", "Doc3"];
+  const [audit, setAudit] = useState();
+  const [docs, setDocs] = useState();
 
   // useEffect(() => {
   //   set
   // }, []);
+  useEffect(() => {
+    setAudit(auditData.AuditData);
+  }, [audit]);
+
+  console.log(audit);
 
   const handleClick = (name) => {
     if (Object.keys(auditData.AuditData).includes(name))
@@ -78,6 +85,23 @@ const App = () => {
           )}
         </div>
       </Grid>
+
+      <div>
+        {(option === 0 || option === 2) && (
+          <Grid item xs={6}>
+            <Paper elevation={4}>
+              <h4>Document</h4>
+              {Object.values(audit).map((value, index) => {
+                return (
+                  <div key={index}>
+                    <h2>{value}</h2>
+                  </div>
+                );
+              })}
+            </Paper>
+          </Grid>
+        )}
+      </div>
 
       {/*  result part */}
 
